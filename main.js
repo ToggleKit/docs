8c21664f889a62aee265654d3c54ac4c43217ae0
Toggle Kit <165154534+ToggleKit@users.noreply.github.com>
2024-09-07 19:22:28 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,112 @@
+window.addEventListener('hashchange', () => {
+    hashChange();
+    removeClassActive();
+    afterLoadContent();
+});
+window.addEventListener('load', () => {
+    hashChange();
+    removeClassActive();
+    afterLoadContent();
+});
 
-function divHide(id) {
-    const divs = document.querySelectorAll(".section");
-    for (let div of divs) {
-        div.style.display = "none";
+
+async function getText(path) {
+  try {
+    let res = await fetch(path);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
     }
-    document.getElementById(id).style.display = "block";
+    let content = await res.text();
+    document.querySelector('#app').innerHTML = content;
+    prism();
+    toggleKit();
+  } catch (error) {
+    console.error('Error fetching the text:', error);
+    document.querySelector('#app').innerHTML = '<p>Failed to load content. Please try again later.</p>';
+  }
 }
 
-const asideP = document.querySelectorAll("aside>p");
 
-function ps() {
-    for (let p of asideP) {
-        p.classList.remove("select","ani");
-    }
+
+function hashChange() {
+    let hash = window.location.hash;
+    let url = hash ? hash.replace('#', '') + '.html' : 'home.html';
+    getText(url);
 }
-
-for (let p of asideP) {
-    p.addEventListener('click', function() {
-        ps();
-        p.classList.add("select","ani");
-    });
-};
 
 function copyToClipboard(cls) {
-    const txt = document.querySelector('.'+cls).textContent
+    const txt = document.querySelector('.' + cls).textContent
     navigator.clipboard.writeText(txt).then(() => {
-      alert('Code copied to clipboard!');
+        notify();
     }).catch(err => {
-      console.error('Failed to copy: ', err);
+        console.error('Failed to copy: ', err);
     });
 };
-const menu = document.querySelector(".menu");
-menu.onclick = function(){
-  document.querySelector("aside").classList.toggle("active");
-  console.log('hello', document.querySelector("aside").outerHTML)
+function notify(){
+  const notification = document.querySelector('#popup');
+  notification.classList.add('show');
+  setTimeout(()=>{notification.classList.remove('show');}, 3000)
 }
-
-divHide('getStarted');
+const menu = document.querySelector(".menu");
+menu.onclick = function() {
+  menu.classList.toggle('active');
+  document.querySelector("aside").classList.toggle("show");
+}
+document.addEventListener('click',(e)=>{
+  if(e.target !== menu){
+    menu.classList.remove('active');
+    document.querySelector("aside").classList.remove("show");
+  }
+})
+const modeBtn = document.getElementById('mode');
+const body = document.body;
+modeBtn.addEventListener('click', (e) => {
+  const currentmode = localStorage.getItem('mode');
+   
+  if (currentmode) {
+    modeBtn.classList.add('darkmode')
+    localStorage.removeItem('mode');
+    modeBtn.innerHTML = 'brightness_4';
+    body.classList.add('dark')
+  } else {
+    modeBtn.classList.remove('darkmode')
+    localStorage.setItem('mode', 'active');
+    modeBtn.innerHTML = 'light_mode';
+    if(body.classList.contains('dark')){
+      body.classList.remove('dark');
+    }
+  }
+});
+const currentMode = localStorage.getItem('mode');
+if (currentMode) {
+  modeBtn.classList.remove('darkmode')
+  modeBtn.innerHTML = 'light_mode';
+  
+} else {
+  modeBtn.classList.add('darkmode')
+  modeBtn.innerHTML = 'brightness_4';
+  body.classList.add('dark');
+}
+let navs = document.querySelectorAll('aside>a');
+navs.forEach(nav=>{
+  nav.addEventListener('click',()=>{
+    removeClassActive();
+    nav.classList.add('active');
+  })
+})
+function removeClassActive() {
+  navs.forEach(nav => {
+    nav.classList.remove('active');
+  });
+}
+function afterLoadContent(){
+  const hash = window.location.hash
+  navs.forEach(nav => {
+    const href = nav.getAttribute('href');
+    if(hash === href){
+      nav.classList.add('active');
+    }else if(!hash){
+      document.querySelector('aside>a').classList.add('active')
+    }
+});
+}
